e5ba40442d95cc9494a06547b2e0789db6cba4de
cloudliner <cloudliner.jp@gmail.com>
2019-11-30 16:04:48 +0900

    add createTable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,76 @@
 import * as functions from 'firebase-functions';
-import { BigQuery } from '@google-cloud/bigquery';
+import { BigQuery, Dataset, Table, TableMetadata, TableField } from '@google-cloud/bigquery';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+const DATASET_ID = 'mixidea_data';
+const TABLE_ID = 'client_log';
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
+});
+
+export const createTable = functions.https.onRequest(async(request, response) => {
+  const bigquery = new BigQuery();
+
+  let targetDataset: Dataset | null = null;
+
+  const [datasets] = await bigquery.getDatasets();
+  console.log('Datasets:');
+  datasets.forEach(dataset => {
+    console.log(`  ${dataset.id}`);
+    if (dataset.id === DATASET_ID) {
+      targetDataset = dataset;
+    }
+  });
+
+  if (! targetDataset) {
+    const options = {
+      location: 'US',
+    };
+    const [dataset] = await bigquery.createDataset(DATASET_ID, options);
+    console.log(`Dataset Created: ${dataset.id}`);
+    targetDataset = dataset;
+  }
+
+  let targetTable: Table | null = null;
+  const [tables] = await targetDataset.getTables();
+  console.log('Tables:');
+  tables.forEach(table => {
+    console.log(`  ${table.id}`);
+    if (table.id === TABLE_ID) {
+      targetTable = table;
+    }
+  });
+  if (! targetTable) {
+    const schema: TableField[] = [
+      { name: 'level', type: 'STRING' },
+      { name: 'user_name', type: 'STRING' },
+      { name: 'file_name', type: 'STRING' },
+      { name: 'message', type: 'STRING' },
+      { name: 'type', type: 'STRING' },
+      { name: 'server_time', type: 'TIMESTAMP', mode: 'REQUIRED'},
+      { name: 'client_time', type: 'TIMESTAMP', mode: 'REQUIRED'},
+      { name: 'event_id', type: 'STRING' },
+      { name: 'user_id', type: 'STRING' },
+      { name: 'tech', type: 'STRING' },
+      { name: 'module', type: 'STRING' },
+      { name: 'element', type: 'STRING' },
+      { name: 'send_type', type: 'STRING' },
+      { name: 'browser', type: 'STRING' },
+      { name: 'useragent', type: 'STRING' },
+      { name: 'trace', type: 'STRING' }
+    ];
+    const timePartitioning = {
+      field: 'server_time'
+    };
+    const options: TableMetadata = {
+      schema: schema,
+      timePartitioning: timePartitioning
+    };
+    const [table] = await targetDataset.createTable(TABLE_ID, options);
+    console.log(`Table Created: ${table.id}`);
+    targetTable = table;
+  }
+  response.send(`Dataset: ${targetDataset.id}, Table: ${targetTable.id}`);
 });
 
 export const getClientLog = functions.https.onRequest(async(request, response) => {
@@ -32,7 +97,7 @@ export const getClientLog = functions.https.onRequest(async(request, response) =
     const output = `${user_name}, ${message}`;
     console.log(output);
   });
-  response.send(rows)
+  response.send(rows);
 });
 
 /*
